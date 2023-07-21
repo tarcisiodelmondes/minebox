2ecbbb83c18b3ac320dfde0c6e4a98de99ffd96f
Tarcísio Delmondes <tarcisiodelmondes@gmail.com>
2023-07-21 11:55:29 -0300

    Initial commit

---
+++ src/main/java/dev/tarcisio/minebox/MineboxApplication.java
@@ -0,0 +1,13 @@
+package dev.tarcisio.minebox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MineboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MineboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/tarcisio/minebox/MineboxApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.tarcisio.minebox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MineboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
