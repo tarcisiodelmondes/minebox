b06f2df87af65623dc6f27c6b5d4e08fae12a819
Tarcísio Delmondes <tarcisiodelmondes@gmail.com>
2023-08-10 08:41:00 -0300

    feat: Criado repositorio do usuario

---
+++ src/main/java/dev/tarcisio/minebox/repositories/UserRepository.java
@@ -0,0 +1,14 @@
+package dev.tarcisio.minebox.repositories;
+
+import dev.tarcisio.minebox.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByEmail(String email);
+
+    Boolean existsByEmail(String email);
+}
