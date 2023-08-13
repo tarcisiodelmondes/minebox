d62918edbee124f3a7ed9ac139b223bd26ddaab3
Tarcísio Delmondes <tarcisiodelmondes@gmail.com>
2023-08-13 16:54:22 -0300

    feat: adicionado documentação com Swagger

---
+++ src/main/java/dev/tarcisio/minebox/config/OpenApiConfig.java
@@ -0,0 +1,44 @@
+package dev.tarcisio.minebox.config;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
+
+@Configuration
+public class OpenApiConfig {
+    @Value("${swagger.dev-url}")
+    private String devUrl;
+
+    @Value("${swagger.prod-url}")
+    private String prodUrl;
+    
+    @Bean
+    public OpenAPI myOpenApi() {
+       Server devServer = new Server();
+       devServer.setUrl(devUrl);
+       devServer.setDescription("Server URL in Development enviroment");
+
+       Server prodServer = new Server();
+       prodServer.setUrl(prodUrl);
+       prodServer.setDescription("Server URL in Production enviroment");
+
+       Contact contact = new Contact();
+       contact.setEmail("tarcisiodelmondes@gmail.com");
+       contact.setName("Tarcisio");
+       contact.setUrl("https://minebox.tarcisiodelmondes.dev");
+
+       License mitLicense = new License().name("MIT License").url("https://choosealicense.com/licenses/mit/");
+
+       Info info = new Info().title("Minebox API").version("1.0").contact(contact).license(mitLicense);
+
+       return new OpenAPI().info(info).servers(List.of(devServer, prodServer));
+  }
+}
--- src/main/java/dev/tarcisio/minebox/controllers/AuthController.java
+++ src/main/java/dev/tarcisio/minebox/controllers/AuthController.java
@@ -12,7 +12,14 @@ import dev.tarcisio.minebox.services.AuthService;
 import dev.tarcisio.minebox.services.RefreshTokenService;
 
 import dev.tarcisio.minebox.services.UserService;
+import dev.tarcisio.minebox.advice.ArgumentValidateMessage;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -24,50 +31,84 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Auth", description = "Rotas de authenticação e criação de usuario")
 @CrossOrigin(maxAge = 3600)
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
-    @Autowired
-    private UserService userService;
+  @Autowired
+  private UserService userService;
 
-    @Autowired
-    private AuthService authService;
+  @Autowired
+  private AuthService authService;
 
-    @Autowired
-    private RefreshTokenService refreshTokenService;
+  @Autowired
+  private RefreshTokenService refreshTokenService;
 
-    @PostMapping("/signin")
-    public ResponseEntity<?>
-    authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
-        try {
-            JwtResponse authenticate = authService.authenticate(loginRequest);
-            return ResponseEntity.ok(authenticate);
-        } catch (BadCredentialsException e) {
-            return ResponseEntity.status(400).body(e.getMessage());
-        }
+  @Operation(summary = "Rota de login do usuario",
+      description = "Recebe o email e password pelo body. A resposta é um objeto JwtResponse que contem o token, refreshtoken, id, name, email e tokenType",
+      tags = {"authenticação", "post"})
+  @ApiResponses({
+      @ApiResponse(responseCode = "200",
+          content = {@Content(schema = @Schema(implementation = JwtResponse.class),
+              mediaType = "application/json")}),
+      @ApiResponse(responseCode = "400", description = "Credenciais incorretas",
+          content = {@Content(mediaType = "text/plain")}),
+      @ApiResponse(responseCode = "400",
+          description = "Email ou Password mal formatado/nulo \t\n Credenciais incorretas (body=text/plain)",
+          content = {@Content(schema = @Schema(implementation = ArgumentValidateMessage.class),
+              mediaType = "application/json")})})
+  @PostMapping("/signin")
+  public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
+    try {
+      JwtResponse authenticate = authService.authenticate(loginRequest);
+      return ResponseEntity.ok(authenticate);
+    } catch (BadCredentialsException e) {
+      return ResponseEntity.status(400).body(e.getMessage());
     }
+  }
 
-    @PostMapping("/signup")
-    public ResponseEntity<?>
-    registerUser(@Valid @RequestBody SignupRequest signupRequest) {
-        try {
-            userService.createUser(signupRequest);
-            return ResponseEntity.ok(new MessageResponse("User registered successfully"));
-        } catch (EmailAreadyExistsException e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new MessageResponse("Error: Email is already in use"));
-        }
+  @Operation(summary = "Rota de criação de usuario",
+      description = "Recebe name, email e password pelo body. A resposta é um texto puro",
+      tags = {"criar", "post", "usuario"})
+  @ApiResponses({@ApiResponse(responseCode = "201", content = {@Content(mediaType = "text/plain")}),
+      @ApiResponse(responseCode = "400",
+          description = "Email já esta em uso \t\n name, email ou password mal formatado/nulo (ArgumentValidateMessage)",
+          content = {@Content(schema = @Schema(implementation = MessageResponse.class),
+              mediaType = "application/json")})})
+  @PostMapping("/signup")
+  public ResponseEntity<?> registerUser(@Valid @RequestBody SignupRequest signupRequest) {
+    try {
+      userService.createUser(signupRequest);
+      return ResponseEntity.status(201).body(new MessageResponse("User registered successfully"));
+    } catch (EmailAreadyExistsException e) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+          .body(new MessageResponse("Error: Email is already in use"));
     }
+  }
 
-    @PostMapping("/refreshtoken")
-    public ResponseEntity<?> refreshtoken(@Valid @RequestBody TokenRefreshRequest request) {
-        String requestRefreshToken = request.getRefreshToken();
+  @Operation(summary = "Rota que cria um novo token baseado no refresh token",
+      description = "Recebe um refresh token pelo body. A resposta é um objeto TokenRefreshResponse que contém: token, refreshtoken e tokenType",
+      tags = {"refresh token", "post"})
+  @ApiResponses({
+      @ApiResponse(responseCode = "200",
+          content = {@Content(schema = @Schema(implementation = TokenRefreshResponse.class),
+              mediaType = "application/json")}),
+      @ApiResponse(responseCode = "400",
+          description = "Refresh token não existe no banco de dados \t\n refreshtoken mal formatado/nulo (ArgumentValidateMessage)",
+          content = {@Content(schema = @Schema(implementation = MessageResponse.class),
+              mediaType = "application/json")})})
+  @PostMapping("/refreshtoken")
+  public ResponseEntity<?> refreshtoken(@Valid @RequestBody TokenRefreshRequest request) {
+    String requestRefreshToken = request.getRefreshToken();
 
-        try {
-            TokenRefreshResponse tokenRefreshResponse = refreshTokenService.refreshToken(requestRefreshToken);
-            return ResponseEntity.ok(tokenRefreshResponse);
-        } catch (TokenRefreshException e) {
-            return ResponseEntity.status(400).body(new MessageResponse("Error: Token is not in the database!"));
-        }
+    try {
+      TokenRefreshResponse tokenRefreshResponse =
+          refreshTokenService.refreshToken(requestRefreshToken);
+      return ResponseEntity.ok(tokenRefreshResponse);
+    } catch (TokenRefreshException e) {
+      return ResponseEntity.status(400)
+          .body(new MessageResponse("Error: Token is not in the database!"));
     }
+  }
 }
