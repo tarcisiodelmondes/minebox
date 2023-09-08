6932a958404735ff41311031a57a24e76be1245f
Tarcísio Delmondes <tarcisiodelmondes@gmail.com>
2023-09-08 17:13:32 -0300

    feat: adicionado configuração do cors

--- src/main/java/dev/tarcisio/minebox/security/WebSecurityConfig.java
+++ src/main/java/dev/tarcisio/minebox/security/WebSecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -25,83 +26,81 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @Configuration
 @EnableMethodSecurity
 public class WebSecurityConfig {
-    @Autowired
-    UserDetailsServiceImpl userDetailsService;
+  @Autowired
+  UserDetailsServiceImpl userDetailsService;
 
-    @Autowired
-    private AuthEntryPointJwt unauthorizedhandler;
+  @Autowired
+  private AuthEntryPointJwt unauthorizedhandler;
+
+  @Bean
+  public AuthTokenFilter authenticationJwtTokenFilter() {
+    return new AuthTokenFilter();
+  }
+
+  @Bean
+  public DaoAuthenticationProvider authenticationProvider() {
+    DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+
+    authProvider.setUserDetailsService(userDetailsService);
+    authProvider.setPasswordEncoder(passwordEncoder());
+
+    return authProvider;
+  }
+
+  @Bean
+  public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig)
+      throws Exception {
+    return authConfig.getAuthenticationManager();
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http.csrf(csrf -> csrf.disable());
+    http.cors(Customizer.withDefaults());
+    http.exceptionHandling(
+        exception -> exception.authenticationEntryPoint(unauthorizedhandler));
+
+    http.sessionManagement(
+        session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+    http.authorizeHttpRequests(
+        auth -> auth.requestMatchers(AntPathRequestMatcher.antMatcher("/api/auth/**")).permitAll()
+            .requestMatchers(
+                AntPathRequestMatcher.antMatcher("/swagger-ui/**"),
+                AntPathRequestMatcher.antMatcher("/v3/api-docs/**"),
+                AntPathRequestMatcher.antMatcher("/v2/api-docs/**"),
+                AntPathRequestMatcher.antMatcher("/swagger-resources/**"))
+            .permitAll()
+            .anyRequest()
+            .authenticated());
+
+    http.authenticationProvider(authenticationProvider());
+
+    http.addFilterBefore(authenticationJwtTokenFilter(),
+        UsernamePasswordAuthenticationFilter.class);
+    ;
+
+    return http.build();
+  }
+
+  @Profile("test")
+  @Configuration(proxyBeanMethods = false)
+  public class DevProfileSecurityConfiguration {
 
     @Bean
-    public AuthTokenFilter authenticationJwtTokenFilter() {
-        return new AuthTokenFilter();
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    SecurityFilterChain h2ConsoleSecurityFilterChain(HttpSecurity http)
+        throws Exception {
+      http.securityMatcher(PathRequest.toH2Console());
+      http.csrf((csrf) -> csrf.disable());
+      http.headers(
+          (headers) -> headers.frameOptions((frame) -> frame.sameOrigin()));
+      return http.build();
     }
-
-    @Bean
-    public DaoAuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-
-        authProvider.setUserDetailsService(userDetailsService);
-        authProvider.setPasswordEncoder(passwordEncoder());
-
-        return authProvider;
-    }
-
-    @Bean
-    public AuthenticationManager
-    authenticationManager(AuthenticationConfiguration authConfig)
-            throws Exception {
-        return authConfig.getAuthenticationManager();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable());
-
-        http.exceptionHandling(
-                exception -> exception.authenticationEntryPoint(unauthorizedhandler));
-
-        http.sessionManagement(
-                session
-                        -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-
-        http.authorizeHttpRequests(
-                auth
-                        -> auth.requestMatchers(AntPathRequestMatcher.antMatcher("/api/auth/**")).permitAll()
-                        .requestMatchers(
-                          AntPathRequestMatcher.antMatcher("/swagger-ui/**"),
-                          AntPathRequestMatcher.antMatcher("/v3/api-docs/**"),
-                          AntPathRequestMatcher.antMatcher("/v2/api-docs/**"),
-                          AntPathRequestMatcher.antMatcher("/swagger-resources/**")
-                         ).permitAll()
-                        .anyRequest()
-                        .authenticated());
-
-        http.authenticationProvider(authenticationProvider());
-
-        http.addFilterBefore(authenticationJwtTokenFilter(),
-                UsernamePasswordAuthenticationFilter.class);
-
-        return http.build();
-    }
-
-    @Profile("test")
-    @Configuration(proxyBeanMethods = false)
-    public class DevProfileSecurityConfiguration {
-
-        @Bean
-        @Order(Ordered.HIGHEST_PRECEDENCE)
-        SecurityFilterChain h2ConsoleSecurityFilterChain(HttpSecurity http)
-                throws Exception {
-            http.securityMatcher(PathRequest.toH2Console());
-            http.csrf((csrf) -> csrf.disable());
-            http.headers(
-                    (headers) -> headers.frameOptions((frame) -> frame.sameOrigin()));
-            return http.build();
-        }
-    }
+  }
 }
