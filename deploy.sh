47298d8a6c2a5bb2c7d70876afc1f88defd1c2a6
Tarcísio Delmondes <tarcisiodelmondes@gmail.com>
2023-10-04 22:11:54 -0300

    ci: Adiciona fluxo de CI/CD

---
+++ deploy.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+start=$(date +"%s")
+
+ssh -p ${SERVER_PORT} ${SERVER_USER}@${SERVER_HOST} -i key.txt -t -t -o StrictHostKeyChecking=no <<'ENDSSH'
+docker pull tarcisiodelmondes/minebox:latest
+
+CONTAINER_NAME=minebox
+if [ "$(docker ps -qa -f name=$CONTAINER_NAME)" ]; then
+	if [ "$(docker ps -q -f name=$CONTAINER_NAME)" ]; then
+		echo "Container is running -> stopping it..."
+		docker stop $CONTAINER_NAME
+	fi
+fi
+
+docker run --network=host -d --rm -p 8080:8080 \
+  -e AWS_REGION \
+  -e AWS_ACCESS_KEY_ID \
+  -e AWS_SECRET_ACCESS_KEY \
+  -e BUCKET_NAME \
+  -e AWS_REGION \
+  -e SENTRY_AUTH_TOKEN \
+  -e SENTRY_DSN \
+  -e SENTRY_ENABLED \
+  -e APP_PROFILE=prod \
+  -e POSTGRES_PASSWORD \
+  -e POSTGRES_USER \
+  -e POSTGRES_DB \
+  -e DATABASE_URL \
+  -e JWT_SECRET \
+  -e JWT_EXPIRATION \
+  -e JWT_REFRESH_EXPIRATION \
+  -e CORS_ORIGINS \
+  --name $CONTAINER_NAME tarcisiodelmondes/minebox:latest
+
+exit
+ENDSSH
+
+if [ $? -eq 0 ]; then
+	exit 0
+else
+	exit 1
+fi
+
+end=$(data +"%s")
+
+diff=$(($end - $start))
+
+echo "Deployed in: ${diff}s"
